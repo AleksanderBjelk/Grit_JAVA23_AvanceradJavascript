b92f7e0fe589edbc41502fb21f701cd6aa6f15d9
AleksanderBjelk <bjelkaleksander@gmail.com>
2024-05-22 09:32:59 +0200

    lektion 9 push 1

---
+++ lektion 9/src/main.ts
@@ -0,0 +1,17 @@
+let word = 'word';
+
+word = 45;
+
+console.log(word);
+
+let arr: number[] = [45, 32];
+
+let arr2: [number,number,number] = [1,2,3];
+
+arr2[3] = 4;
+
+arr2.push(4);
+
+// let arr2: [] = [];
+// arr2.push(4, 'hej',3,'fem');
+console.log(arr2);
